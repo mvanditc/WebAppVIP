5582cb02d26b9cb6addb7e2e0cdbe874da2e5ac3
Sehaj Naangal <snaangal@uwo.ca>
2023-11-29 23:37:12 -0500

    add scan label and target text and scanning bar using ascii loader library

--- js/scanPage.js
+++ js/scanPage.js
@@ -1,1 +1,25 @@
 // JavaScript used for the Scanning Page's functionality.
+
+var progressBar = new AsciiProgress("progressbar",{
+  openCharacter: "[ ",
+  loadedCharacter: "#",
+  backgroundCharacter: "-",
+  closeCharacter: " ]",
+  length: 40,
+  value: 0,
+  completeAt: 100,
+  showPercent: false,
+  percentDecimalPlaces: 2,
+  percentLocation: "top",
+  showComment: true,
+  startingComment: " ",
+  commentLocation: "bottom"
+});
+
+const scanPercent = 0;
+for (let i = 0; i < 101; i = i + 5) {
+  setTimeout(function() {
+    progressBar.setValue(i);
+    progressBar.setComment(i + "%");
+  }, i*100)
+}
