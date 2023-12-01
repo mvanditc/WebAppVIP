1aea64d10cdeedeaabbdd5510163f2c578d4924a
HereisDisco <jmarroqu@uwo.ca>
2023-11-30 19:08:50 -0500

    Task 1 Homepage
    Just a basic counter and blurb of text

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // JavaScript used for the Homepage's Counter functionality.
+
+//Basic counter//
+let counter = 0;
+
+function incrementCounter() {
+  counter++;
+  document.getElementById('counter').innerText = counter;
+}
