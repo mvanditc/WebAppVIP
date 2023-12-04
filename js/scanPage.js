d9d077c6850506c8f09ba3524035119a27d4f410
Sehaj Naangal <snaangal@uwo.ca>
2023-12-04 09:47:18 -0500

    add - symbol inside progress bar

--- js/scanPage.js
+++ js/scanPage.js
@@ -4,11 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const progressBar = document.getElementById('scanPage-progressBar');
   const progressBarPercentage = document.getElementById('scanPage-progressBar-percentage');
   const finalPercentage = 100;
-  let loadingSymbol = "";
+  let loadingSymbol = "--".repeat(100/5) + 1;
   
   for (let i = 0; i < finalPercentage + 1; i = i + 5) {
     setTimeout(() => {
-      loadingSymbol += "█";
+      loadingSymbol = "█" + loadingSymbol;
+      loadingSymbol = loadingSymbol.slice(0, -2);
       progressBar.textContent = `[ ${loadingSymbol} ]`;
       progressBarPercentage.textContent = `${i}%`;
     }, i * 50);
