f84d02b8f52a6655a01130edd09746a6a9a1050e
Sehaj Naangal <snaangal@uwo.ca>
2023-12-04 09:31:48 -0500

    create terminal loading bar using textContent

--- js/scanPage.js
+++ js/scanPage.js
@@ -1,12 +1,16 @@
 // JavaScript used for the Scanning Page's functionality.
 
-const progressBar = document.getElementById('scanPage-progress-bar');
-const finalPercentage = 100;
-
-const increaseProgress = () => {
-  for (let i = 0; i < finalPercentage + 1; i++) {
+document.addEventListener('DOMContentLoaded', function() {
+  const progressBar = document.getElementById('scanPage-progressBar');
+  const progressBarPercentage = document.getElementById('scanPage-progressBar-percentage');
+  const finalPercentage = 100;
+  let loadingSymbol = "";
+  
+  for (let i = 0; i < finalPercentage + 1; i = i + 5) {
     setTimeout(() => {
-      progressBar.innerHTML = "#";
+      loadingSymbol += "█";
+      progressBar.textContent = `[ ${loadingSymbol} ]`;
+      progressBarPercentage.textContent = `${i}%`;
     }, i * 50);
   }
-}
+})
