15b982f18467b943d966f6212bffad9c77a1f458
Sehaj Naangal <snaangal@uwo.ca>
2023-12-04 16:20:07 -0500

    update time elpased based on progress

--- js/scanPage.js
+++ js/scanPage.js
@@ -1,17 +1,23 @@
 // JavaScript used for the Scanning Page's functionality.
 
-document.addEventListener('DOMContentLoaded', function() {
-  const progressBar = document.getElementById('scanPage-progressBar');
-  const progressBarPercentage = document.getElementById('scanPage-progressBar-percentage');
-  const finalPercentage = 100;
-  let loadingSymbol = "--".repeat(100/5) + 1;
-  
-  for (let i = 0; i < finalPercentage + 1; i = i + 5) {
-    setTimeout(() => {
-      loadingSymbol = "█" + loadingSymbol;
-      loadingSymbol = loadingSymbol.slice(0, -2);
-      progressBar.textContent = `[ ${loadingSymbol} ]`;
-      progressBarPercentage.textContent = `${i}%`;
-    }, i * 50);
-  }
-})
+const progressBar = document.getElementById('scanPage-progressBar');
+const progressBarPercentage = document.getElementById('scanPage-progressBar-percentage');
+const finalPercentage = 100;
+let loadingSymbol = "--".repeat(100/5) + 1;
+
+const timeElapsed = document.getElementById('scanPage-time-elapsed');
+let seconds = 55;
+let minutes = 0;
+
+for (let i = 0; i < finalPercentage + 1; i = i + 5) {
+  setTimeout(() => {
+    loadingSymbol = "█" + loadingSymbol;
+    loadingSymbol = loadingSymbol.slice(0, -2);
+    progressBar.textContent = `[ ${loadingSymbol} ]`;
+    progressBarPercentage.textContent = `${i}%`;
+
+    seconds += 1;
+    minutes = Math.floor(seconds/60);
+    timeElapsed.textContent = `${minutes}:${seconds}`
+  }, i * 200);
+}
