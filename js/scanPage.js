afc9e33589f7aab659ac8a40927fa74ee8d2c8a8
Sehaj Naangal <snaangal@uwo.ca>
2023-12-06 11:30:51 -0500

    shorten progress bar width

--- js/scanPage.js
+++ js/scanPage.js
@@ -3,14 +3,26 @@
 const progressBar = document.getElementById('scanPage-progressBar');
 const progressBarPercentage = document.getElementById('scanPage-progressBar-percentage');
 const finalPercentage = 100;
-let loadingSymbol = "--".repeat(100/5) + 1;
+const percentageJump = 10;
+
+let screenWidth = window.innerWidth;  
+let loadingSymbol = "--".repeat(Math.floor(100/percentageJump)) + 1;
+
+// const updateScreenWidth = () => {
+//   screenWidth = window.innerWidth;  
+//   loadingSymbol = "--".repeat(Math.floor(screenWidth/30)) + 1;
+// }
+
+// updateScreenWidth();
+// window.addEventListener('resize', updateScreenWidth);
+
 
 const timeElapsed = document.getElementById('scanPage-time-elapsed');
 let seconds = -1;
 let minutes = 0;
 let screenSeconds = seconds;
 
-for (let i = 0; i < finalPercentage + 1; i = i + 5) {
+for (let i = 0; i < finalPercentage + 1; i = i + percentageJump) {
   setTimeout(() => {
     loadingSymbol = "█" + loadingSymbol;
     loadingSymbol = loadingSymbol.slice(0, -2);
