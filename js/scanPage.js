ddf5c5a421799cdc4503e762eca8caa02a22de63
Sehaj Naangal <snaangal@uwo.ca>
2023-12-07 03:14:15 -0500

    add promise to complete scan then show vulnerabilities

--- js/scanPage.js
+++ js/scanPage.js
@@ -2,28 +2,27 @@
 
 const progressBar = document.getElementById('scanPage-progressBar');
 const progressBarPercentage = document.getElementById('scanPage-progressBar-percentage');
+
 const finalPercentage = 100;
 const percentageJump = 10;
 
-let screenWidth = window.innerWidth;  
 let loadingSymbol = "--".repeat(Math.floor(100/percentageJump)) + 1;
 
-// const updateScreenWidth = () => {
-//   screenWidth = window.innerWidth;  
-//   loadingSymbol = "--".repeat(Math.floor(screenWidth/30)) + 1;
-// }
-
-// updateScreenWidth();
-// window.addEventListener('resize', updateScreenWidth);
-
+const lowRisk = document.getElementById('scanPage-low-risk');
+const mediumRisk = document.getElementById('scanPage-medium-risk');
+const highRisk = document.getElementById('scanPage-high-risk');
 
 const timeElapsed = document.getElementById('scanPage-time-elapsed');
 let seconds = -1;
 let minutes = 0;
 let screenSeconds = seconds;
 
-for (let i = 0; i < finalPercentage + 1; i = i + percentageJump) {
-  setTimeout(() => {
+lowRisk.textContent = "-";
+mediumRisk.textContent = "-";
+highRisk.textContent = "-";
+
+const performScan = async () => {
+  for (let i = 0; i < finalPercentage + 1; i = i + percentageJump) {
     loadingSymbol = "█" + loadingSymbol;
     loadingSymbol = loadingSymbol.slice(0, -2);
     progressBar.textContent = `[ ${loadingSymbol} ]`;
@@ -38,5 +37,13 @@ for (let i = 0; i < finalPercentage + 1; i = i + percentageJump) {
 
     minutes = Math.floor(seconds/60);
     timeElapsed.textContent = `${minutes}:${screenSeconds}`
-  }, i * 100);
+
+    await new Promise((resolve) => setTimeout(resolve, percentageJump*20));
+  }
 }
+
+performScan().then(() => {
+  lowRisk.textContent = String(1);
+  mediumRisk.textContent = String(2);
+  highRisk.textContent = String(3);
+})
