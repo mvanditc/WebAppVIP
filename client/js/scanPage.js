be7e7248bda02baec10974dfefba2513fba1798d
Michael Vanditch <mvanditc@uwo.ca>
2024-01-15 13:23:00 -0500

    Scan and Scan Results page added

--- client/js/scanPage.js
+++ client/js/scanPage.js
@@ -1,1 +1,53 @@
 // JavaScript used for the Scanning Page's functionality.
+
+const progressBar = document.getElementById('scanPage-progressBar');
+const progressBarPercentage = document.getElementById('scanPage-progressBar-percentage');
+const viewDetails = document.getElementById('scanPage-view-details-container');
+
+const finalPercentage = 100;
+const percentageJump = 10;
+
+let loadingSymbol = "--".repeat(Math.floor(100/percentageJump)) + 1;
+
+const lowRisk = document.getElementById('scanPage-low-risk');
+const mediumRisk = document.getElementById('scanPage-medium-risk');
+const highRisk = document.getElementById('scanPage-high-risk');
+
+const timeElapsed = document.getElementById('scanPage-time-elapsed');
+let seconds = -1;
+let minutes = 0;
+let screenSeconds = seconds;
+
+lowRisk.textContent = "-";
+mediumRisk.textContent = "-";
+highRisk.textContent = "-";
+
+viewDetails.style.display = 'none';
+
+const performScan = async () => {
+  for (let i = 0; i < finalPercentage + 1; i = i + percentageJump) {
+    loadingSymbol = "█" + loadingSymbol;
+    loadingSymbol = loadingSymbol.slice(0, -2);
+    progressBar.textContent = `[ ${loadingSymbol} ]`;
+    progressBarPercentage.textContent = `${i}%`;
+    
+    seconds += 1;
+    screenSeconds += 1;
+    
+    if (seconds % 60 === 0) {
+      screenSeconds = 0;
+    }
+
+    minutes = Math.floor(seconds/60);
+    timeElapsed.textContent = `${minutes}:${screenSeconds}`
+
+    await new Promise((resolve) => setTimeout(resolve, percentageJump*20));
+  }
+}
+
+performScan().then(() => {
+  lowRisk.textContent = String(1);
+  mediumRisk.textContent = String(2);
+  highRisk.textContent = String(3);
+  viewDetails.style.display = 'block'
+})
