1c09835ede5e89f8ad17ed809b70d8e4c456495c
Michael Vanditch <mvanditc@uwo.ca>
2024-03-08 13:27:02 -0500

    start of MichaelxJosh work session

--- js/languageSelector.js
+++ js/languageSelector.js
@@ -3,6 +3,8 @@ let languageSelectorModal = document.getElementById("languageSelectorModal")
 let languageSelectorApplyButton = document.getElementById("languageSelectorApplyButton")
 let languageSelectorCancelButton = document.getElementById("languageSelectorCancelButton")
 
+let languageSelectorInput = document.getElementById("languageSelectorInput")
+
 let languageSelectorContent = document.getElementById("languageSelectorContent")
 
 function closeLanguageSelectorModal(){
@@ -21,10 +23,27 @@ languageSelectorButton.addEventListener("click", openLanguageSelectorModal)
 
 languageSelectorCancelButton.addEventListener("click", closeLanguageSelectorModal)
 
-languageSelectorApplyButton.addEventListener("click", closeLanguageSelectorModal)
+languageSelectorApplyButton.addEventListener("click", ()=>{
+    applyTranslation()
+    //closeLanguageSelectorModal()
+})
 
 languageSelectorModal.addEventListener("click", (event)=>{
     if (event.target == languageSelectorModal){
         closeLanguageSelectorModal()
     }
 })
+
+function applyTranslation(){
+    //debugger;
+    
+    const event = new Event("change");
+    let googleTranslateElement = document.getElementById("google_translate_element")
+    let googleTranslateSelect = googleTranslateElement.children[0].children[0].children[0]
+
+    languageSelectorInput.innerHTML = googleTranslateSelect.innerHTML
+
+    googleTranslateSelect.value = "ja"
+
+    googleTranslateSelect.dispatchEvent(event);
+}
